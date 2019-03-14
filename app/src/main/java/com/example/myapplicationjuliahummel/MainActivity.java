33ece1c04ecfbb304f6649f6572fa405dcebc303
Julia Hummel <juliahum@edu.aau.at>
2019-03-14 20:21:22 +0100

    Primzahlberechnung: onclick Methode für Button erstellt (Bei klicken des ButtonPrim werden die Ziffern der Matrikelnummer aufsteigend sortiert und nur die keine Primzahlen sind ausgegeben)

--- app/src/main/java/com/example/myapplicationjuliahummel/MainActivity.java
+++ app/src/main/java/com/example/myapplicationjuliahummel/MainActivity.java
@@ -1,7 +1,28 @@
 package com.example.myapplicationjuliahummel;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +30,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+    //OnClick button
+   /* public void button_onClick(View view) throws Exception {
+        //Matrikelnummereingabe
+        EditText editText =(EditText)findViewById(R.id.editText);
+        String matrikelnr = editText.getText().toString();
+
+        //Senden aufrufen
+        //String antwort = sendEmpfangNeu(matrikelnr).toString();
+
+        //Ausgabefeld
+        TextView textAusgabe =(TextView)findViewById(R.id.textAusgabe);
+        //textAusgabe.setText(antwort);
+
+        //sendenEmpfangen();
+    }*/
+
+
+    //Berechnung Prim
+    public void buttonPrim_onclick(View view) {
+        //Matrikelnummereingabe
+        EditText editText =(EditText)findViewById(R.id.editText);
+        String stringnumber = editText.getText().toString();
+
+
+        int[] ziffern = new int[stringnumber.length()]; //Array anlegen dass so groß wie die Zahl lang ist
+
+        //Zahlen in Array einfügen
+        for (int i = 0; i < ziffern.length; i++) {
+            ziffern[i] = Integer.parseInt(String.valueOf(stringnumber.charAt(i)));
+        }
+
+        //Ziffern sortieren
+        Arrays.sort(ziffern);
+
+        //Primzahl ausgeben
+        List ergebnis = new LinkedList();
+        for (int i = 0; i < ziffern.length; i++) {
+            boolean primTest = isPrime(ziffern[i]);
+            if (!primTest) { //Wenn die Zahl keine Primzahl ist speichere sie in die liste
+                ergebnis.add(ziffern[i]);
+            }
+        }
+
+        //Ausgabe
+        TextView ausgabePrim =(TextView)findViewById(R.id.textViewPrim);
+        ausgabePrim.setText("Matrikelnummer ohne Primzahlen" +ergebnis.toString());
+    }
+
+    public static boolean isPrime(int n) {
+        if(n == 3 || n == 5 || n ==7){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+
+
 }
